f9d72626219377ca2ae9ea7e10959441a2ecc295
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-10 00:06:58 -0500

    contenedor para proyectos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Fragment } from 'react';
 import Hero from './components/Hero';
 import Servicio from './components/Servicio';
+import Proyecto from './components/Proyecto';
+
 import serviciosInfo from './datos/servicioInfo.json';
+import proyectosInfro from './datos/proyectosInfo.json';
 
 function App() {
 
@@ -12,6 +15,12 @@ function App() {
       <div className="servicios contenedor">
         {serviciosInfo.map(infoServicio => <Servicio key = {infoServicio.id} infoServicio = {infoServicio}/>)}
       </div>
+      <main className="portafolio contenedor">
+        <h2>Mi Portafolio</h2>
+        <div id="listado" className="listado">
+          {proyectosInfro.map(proyecto => <Proyecto key = {proyecto.id} proyecto = {proyecto}/>)}
+        </div>
+    </main>
     </Fragment>
   );
 }
