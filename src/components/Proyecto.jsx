e71edb098f79f094e9ef9fafc585317235dacc9a
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-10 01:15:42 -0500

    Componente de proyecto creado

---
+++ src/components/Proyecto.jsx
@@ -0,0 +1,28 @@
+import React, { Fragment } from "react";
+import {} from "../assets/components/Proyecto.css";
+
+const Proyecto = ({ proyecto }) => {
+  const { titulo, descripcion, src, github } = proyecto;
+  return (
+    <Fragment>
+      <div className="proyecto">
+        <img src={require(`../assets/img/${src}`)} alt={titulo} />
+        <div className="proyecto-contenido">
+          <h3>{titulo}</h3>
+          <p>{descripcion}</p>
+          <a
+            href={github}
+            className="btn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Ver Más en GitHub
+          </a>
+        </div>
+      </div>
+      
+    </Fragment>
+  );
+};
+
+export default Proyecto;
