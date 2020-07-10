80d06ec368400b6bddc025f9faa2371ea7c794c0
Alejandro Barba <alejandrobarba742@gmail.com>
2020-07-09 22:36:44 -0500

    Componente Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {} from '../assets/components/Hero.css';
+const Hero = () => {
+    return (
+      <div className="hero">
+        <div className="contenedor">
+          <div className="contenido">
+            <h1 className="titulo">Desarrollo Web</h1>
+            <p>Desarrollador Web</p>
+          </div>
+        </div>
+      </div>
+    );
+}
+ 
+export default Hero;
