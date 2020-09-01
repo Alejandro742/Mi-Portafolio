3795f45ab980b1d18c2df949e386858eb6b6683c
Alejandro Barba <alejandrobarba742@gmail.com>
2020-08-31 21:10:26 -0500

    Navbar responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import Navbar from './Navbar';
+
 import {} from '../assets/components/Hero.css';
 const Hero = () => {
     return (
       <div className="hero">
+        <Navbar/>
         <div className="contenedor">
           <div className="contenido">
             <h1 className="titulo">Alejandro Barba</h1>
-            <p>Desarrollador Web</p>
+            <p>Web Developer</p>
           </div>
         </div>
       </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {} from "../assets/components/Navbar.css";
+const Navbar = () => {
+  function showMenu(e){
+    const navbar = document.querySelector("#navbar");
+    if(navbar.classList.contains("navbar__options--showMenu")){
+      navbar.classList.remove("navbar__options--showMenu");
+    }else{
+      navbar.classList.add("navbar__options--showMenu");
+    }
+  }
+  return (
+    <nav className="navbar">
+      <img
+        className="navbar__burger"
+        alt=""
+        src={require("../assets/img/menu.png")}
+        onClick={showMenu}
+      />
+      <div className="navbar__content">
+        <a href="/#" className="navbar__logo">
+          &lt; Alejandro /&gt;
+        </a>
+        {/* navbar__options--showMenu */}
+        <ul className="navbar__options" id="navbar">
+          <li className="navbar__option">
+            <a href="/#" className="navbar__option-ahref">
+              Home
+            </a>
+          </li>
+          <li className="navbar__option">
+            <a href="/#" className="navbar__option-ahref">
+              About
+            </a>
+          </li>
+          <li className="navbar__option">
+            <a href="/#" className="navbar__option-ahref">
+              Portfolio
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
