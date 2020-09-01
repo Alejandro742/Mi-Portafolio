e51b174e0a4b467a02732a6e10863e1f488bb867
Alejandro Barba <alejandrobarba742@gmail.com>
2020-08-31 22:33:37 -0500

    Hero modificado

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,14 @@ import {} from '../assets/components/Hero.css';
 const Hero = () => {
     return (
       <div className="hero">
+        <div className="hero__shadow">
         <Navbar/>
-        <div className="contenedor">
           <div className="contenido">
+            <img src={require('../assets/img/perfil.jpg')} className="hero__img" alt="prfil_img"/>
             <h1 className="titulo">Alejandro Barba</h1>
-            <p>Web Developer</p>
+            <p className="hero__about">I'am web developer based in Jalisco - Mexico, I'am passionate about tech and learning new things about web development.</p>
           </div>
+
         </div>
       </div>
     );
