ceb2384b2343b83f6aac390901dcfa9650f3e9cf
Alejandro Barba <alejandrobarba742@gmail.com>
2020-08-31 22:50:27 -0500

    Footer mejorado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,35 @@ import {} from '../assets/components/Footer.css'
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="contenedor">
-        <div className="widget">
-          <h3>Contacto</h3>
-          <p>Tel: 3118478456</p>
-          <p>Correo: alejandrobarba742@gmail.com</p>
-          <p><a href="https://www.linkedin.com/in/alejandro-barba-garc%C3%ADa-355090198/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-        </div>
-        <div className="widget">
-          <h3>Sobre Mí</h3>
-          <p className="about-me">Mi nombre es Alejandro Barba, estudiante en Ingeniería en Computación en UDG, tengo 21 años y soy un apasionado por la programación, tecnología y enseñanza. Tengo aproximadamente 1 año de experiencia desarrollando aplicaciones web de manera personal, y un proyecto para la universidad (Asesorías Universitarias).
-          Me oriento más a lado del Backend, pero siempre trato de estar actualizado en FrontEnd</p>
+      <div className="footer__contenedor">
+        <p className="footer__copyright">
+          Hecho por <strong>Aejandro Barba</strong> &copy; 2020
+        </p>
+        <div className="footer__redes">
+          <a 
+            href="https://twitter.com/Alejand78666280"
+            target="_blank" rel="noopener noreferrer"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a 
+            href="https://www.linkedin.com/in/alejandro-barba-garc%C3%ADa-355090198/"
+            target="_blank" rel="noopener noreferrer"  
+          >
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          <a 
+            href="https://github.com/Alejandro742"
+            target="_blank" rel="noopener noreferrer"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+          <a 
+            href="mailto:alejandrobarba742@gmail.com"
+            target="_blank" rel="noopener noreferrer"
+          >
+            <i className="fas fa-envelope"></i>
+          </a>
         </div>
       </div>
     </footer>
